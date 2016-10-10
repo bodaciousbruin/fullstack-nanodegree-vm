67061168c04feba2f95f64aa78f6a035f5f65b4a
Ben Abrams <bodaciousbruin@gmail.com>
2016-10-09 17:31:20 -0700

    adding puppy scripts and db setups

---
+++ vagrant/puppies/puppiesDbSetup.py
@@ -0,0 +1,36 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Numeric, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Shelter(Base):
+    __tablename__ = 'shelter'
+    
+    name = Column( String(80), nullable = False)
+    address = Column( String(100))
+    city = Column(String(100))
+    state = Column(String(50))
+    zipCode = Column(Integer)
+    website = Column(String(250))
+    id = Column(Integer, primary_key = True)
+
+class Puppy(Base):
+    __tablename__ = 'puppy'
+    
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    dateOfBirth = Column(DateTime)
+    gender = Column(String(6))
+    weight = Column(Numeric)
+    picture = Column(String(250))
+    shelter_id = Column(Integer, ForeignKey('shelter.id'))
+    shelter = relationship(Shelter)
+
+
+##################### all code above this line ################
+
+engine = create_engine( 'sqlite:///puppies.db')
+Base.metadata.create_all(engine)
